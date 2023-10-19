9c91ae9e726145433a3786112741cd905621b896
ichsanrafii04 <143162678+ichsanrafii04@users.noreply.github.com>
2023-10-19 20:15:10 +0800

    upload files

---
+++ Assets/smailja.js
@@ -0,0 +1,21 @@
+// NAVBAR
+const ourcourse = document.getElementById("ourcourse-menu");
+const academic = document.getElementById("academic-menu");
+const ekskul = document.getElementById("extracurricular-menu");
+
+function ourcourseBtn() {
+  ourcourse.classList.toggle("our-drop");
+}
+
+function academicBtn() {
+  academic.classList.toggle("aca-drop");
+}
+
+function ekskulBtn() {
+  ekskul.classList.toggle("ex-drop");
+}
+// END OF NAVBAR
+
+// MAIN
+
+// END OF MAIN
